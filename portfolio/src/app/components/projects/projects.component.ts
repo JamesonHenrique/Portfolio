be544fc31acbd7e69592e3d0fc6a01ae9409c01d
Jameson Henrique <122840154+JamesonHenrique@users.noreply.github.com>
2025-06-25 15:21:12 -0300

    feat: adicionar projeto painel de máquinas(freelancer) e trocar para um novo currículo

--- portfolio/src/app/components/projects/projects.component.ts
+++ portfolio/src/app/components/projects/projects.component.ts
@@ -11,11 +11,26 @@ import { Component, HostListener } from '@angular/core';
 export class ProjectsComponent {
   isVisible = false;
   projects = [
+     {
+          title: 'Painel de máquinas',
+          name: 'Freelancer',
+          description: 'Um painel de monitoramento em tempo real para acompanhar o status de máquinas, permitindo a alteração dos estados de cada equipamento.',
+          techs: [
+            'Java','Typescript',
+            'Spring Boot',
+            'Angular',
+            'TailwindCSS',
+            'MongoDB',
+          ],
+          image: 'painel-de-maquinas_print.png',
+          url: 'URL Indisponivel',
+
+        },
     {
       title: 'NEWGRAM',
       name: 'Rede Social Moderna',
       description: 'Plataforma de compartilhamento de fotos e stories, com feed personalizado, sistema de seguidores, sistema de favoritos e criação de posts e destaques.',
-      techs: [
+      techs: ['Java','Typescript',
         'Spring Boot',
         'Angular',
         'TailwindCSS',
@@ -33,7 +48,7 @@ export class ProjectsComponent {
       title: 'ALGAMONEY',
       name: 'Sistema de Gestão Financeira Pessoal',
       description: 'Plataforma full stack de gerenciamento financeiro pessoal com dashboard, relatórios e controle de receitas e despesas.',
-      techs: ['Spring Boot', 'Angular','Chart.js','PostgreSQL','TailwindCSS','JasperReports','JWT'],
+      techs: ['Java','Typescript','Spring Boot', 'Angular','Chart.js','PostgreSQL','TailwindCSS','JasperReports','JWT'],
       image: 'algamoney_print.png',
       url: 'https://github.com/JamesonHenrique/Algamoney'
     },
@@ -42,7 +57,7 @@ export class ProjectsComponent {
       title: 'WAVECHAT',
       name: 'Chat em Tempo Real com WebSocket',
       description: 'Aplicação de chat em tempo real com implementação de WebSocket para comunicação instantânea entre usuários.',
-      techs: ['Spring Boot', 'Angular', 'WebSocket','PostgreSQL', 'Keycloak','Bootstrap','Docker'],
+      techs: ['Java','Typescript', 'Spring Boot', 'Angular', 'WebSocket','PostgreSQL', 'Keycloak','Bootstrap','Docker'],
       image: 'wavechat_print.png',
       url: 'https://github.com/JamesonHenrique/WaveChat'
     },
@@ -50,7 +65,7 @@ export class ProjectsComponent {
       title: 'BOOKLORE',
       name: 'Gerenciador de Biblioteca Pessoal',
       description: 'Plataforma full-stack para gerenciamento de coleções de livros com autenticação via e-mail e sistema de empréstimos.',
-      techs: ['Spring Boot', 'Angular Boot',  'PostgreSQL', 'Docker', 'JWT', 'DaisyUI'],
+      techs: ['Java','Typescript','Spring Boot', 'Angular Boot',  'PostgreSQL', 'Docker', 'JWT', 'DaisyUI'],
       image: 'booklore_print.png',
       url: 'https://github.com/JamesonHenrique/Booklore'
     }
